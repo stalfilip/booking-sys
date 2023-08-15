ce0fb5cba0db10f100baee639fc223e21fbd4bf0
Filip Stål <fistal@UG.KTH.SE>
2023-08-15 12:12:02 +0200

    testing

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Resource, Booking, Payment, Availability
+
+admin.site.register(Resource)
+admin.site.register(Booking)
+admin.site.register(Payment)
+admin.site.register(Availability)
 
 # Register your models here.
